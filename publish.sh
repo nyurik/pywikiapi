087f8dadabdb904c7cec35ae813cbd7218d4c97b
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2020-01-08 19:22:54 -0500

    publish script update

--- publish.sh
+++ publish.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
+set -e
+
+python3 setup.py test -q
 
 rm dist/*
 python3 setup.py sdist bdist_wheel
+
+read -r -p "Tests and build complete. Press [Enter] to publish..."
+
 python3 -m twine upload dist/*
